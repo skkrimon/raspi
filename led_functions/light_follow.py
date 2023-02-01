1268cdb05687b186eb6ea71f468468443e17bd59
skkrimon <75689426+skkrimon@users.noreply.github.com>
2023-02-01 21:41:28 +0100

    Update light_follow.py

--- led_functions/light_follow.py
+++ led_functions/light_follow.py
@@ -3,7 +3,13 @@ from time import sleep
 
 
 def light_follow(strip):
+    initial = True
+    
     for i in range(strip.numPixels()):
+        if not initial:
+            strip.setPixelColor(i - 1, Color(0, 0, 0))
+            strip.show()
+        
         strip.setPixelColor(i, Color(255, 0, 0))
         strip.show()
         sleep(25 / 1000.0)
